514f973c5d0a802409bad34d02d2e932d928b4b4
Vishal <vishal.adhikari36@gmail.com>
2024-12-09 03:13:49 +0530

    add mongodb query & criteria

---
+++ src/main/java/com/vishal/journalbackend/repository/UserRepositoryImpl.java
@@ -0,0 +1,35 @@
+package com.vishal.journalbackend.repository;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+
+import com.vishal.journalbackend.entity.User;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class UserRepositoryImpl {
+
+    private final MongoTemplate mongoTemplate;
+
+    @Autowired
+    public UserRepositoryImpl(MongoTemplate mongoTemplate) {
+        this.mongoTemplate = mongoTemplate;
+    }
+
+    public List<User> getUsersForSentimentAnalysis() {
+        Query query = new Query();
+        query.addCriteria(Criteria.where("email").regex("^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Z|a-z]{2,6}$"));
+        query.addCriteria(Criteria.where("sentimentAnalysis").is(true));
+        List<User> users = mongoTemplate.find(query, User.class);
+        for (User user : users) {
+            log.info("User: ", user.getUsername());
+        }
+        return users;
+    }
+
+}
---
+++ src/test/java/com/vishal/journalbackend/repository/UserRepositoryImplTests.java
@@ -0,0 +1,29 @@
+package com.vishal.journalbackend.repository;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.vishal.journalbackend.entity.User;
+
+@SpringBootTest
+class UserRepositoryImplTests {
+
+    private final UserRepositoryImpl userRepositoryImpl;
+
+    @Autowired
+    public UserRepositoryImplTests(UserRepositoryImpl userRepositoryImpl) {
+        this.userRepositoryImpl = userRepositoryImpl;
+    }
+
+    @Test
+    void testGetUsersForSentimentAnalysis() {
+        List<User> users = userRepositoryImpl.getUsersForSentimentAnalysis();
+        assertTrue(!users.isEmpty());
+    }
+
+}
