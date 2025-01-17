2dab235ba277921a5ee3f381a64295c4a8143538
Vishal <vishal.adhikari36@gmail.com>
2025-01-18 01:46:58 +0530

    endpoint name refactor

--- src/main/java/com/vishal/journalbackend/config/SecurityConfig.java
+++ src/main/java/com/vishal/journalbackend/config/SecurityConfig.java
@@ -34,7 +34,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.authorizeHttpRequests(
                 request -> request
-                        .requestMatchers("/public/**", "/swagger-ui/**", "/v3/api-docs/**", "/oauth/**")
+                        .requestMatchers("/public/**", "/swagger-ui/**", "/v3/api-docs/**", "/auth/**")
                         .permitAll()
                         .requestMatchers("/journal/**", "/user/**").authenticated()
                         .requestMatchers("/admin/**").hasRole("ADMIN")
