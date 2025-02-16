01e89d11619518b05048c254806c7d4793ac4121
Vishal <vishal.adhikari36@gmail.com>
2025-02-16 21:31:00 +0530

    update save new user test data

--- src/test/java/com/vishal/journalbackend/service/UserArgumentsProvider.java
+++ src/test/java/com/vishal/journalbackend/service/UserArgumentsProvider.java
@@ -14,9 +14,9 @@ public class UserArgumentsProvider implements ArgumentsProvider {
     @Override
     public Stream<? extends Arguments> provideArguments(ExtensionContext extensionContext) throws Exception {
         return Stream.of(
-                Arguments.of(User.builder().username("newadmin12345678").password("newadmin")
+                Arguments.of(User.builder().username("newadmin123456789").password("newadmin")
                         .roles(Arrays.asList("USER", "ADMIN")).build()),
-                Arguments.of(User.builder().username("frodo12345678").password("frodo").roles(Arrays.asList("USER")).build()));
+                Arguments.of(User.builder().username("frodo123456789").password("frodo").roles(Arrays.asList("USER")).build()));
     }
 
 }
